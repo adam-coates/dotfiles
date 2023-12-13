09d3e78a98bef0c3e2eefa5eb577e9e2067b2438
Adam Coates <123807847+adamcoatess@users.noreply.github.com>
2023-12-13 21:48:09 +0100

    Added gkeep and chatgpt
    added additional plugins, but see readme for how to use keyring with googleAPI key.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,4 @@
 require("adam.core")
 require("adam.lazy")
 vim.g.python3_host_prog = '/home/linuxbrew/.linuxbrew/bin/python3'
+-- vim.g.python3_host_prog = '/home/linuxbrew/.linuxbrew/bin/python3.8'
---
+++ nvim/lua/adam/plugins/gkeep.lua
@@ -0,0 +1,6 @@
+return {
+    "stevearc/gkeep.nvim",
+    opts = {},
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+}
