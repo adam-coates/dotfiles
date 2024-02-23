22cad2592a03ca98d88ada0c8314d05ba359b98f
Adam Coates <123807847+adamcoatess@users.noreply.github.com>
2024-02-23 16:31:26 +0100

    Only check which OS when vimwiki triggered

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,15 +5,12 @@ require("adam.lazy")
 -- detect os and make change as needed
 local sysname = vim.loop.os_uname().sysname
 if sysname == "Linux" then
-    vim.g.notepath = "/mnt/g/"
     vim.g.python3_host_prog = '/home/linuxbrew/.linuxbrew/bin/python3'
 elseif sysname == "Darwin" then
-    vim.g.notepath = "~/notes/"
+    --vim.g.python3_host_prog = 'already on path using brew'
 else
-    vim.g.notepath = "Windows specific value"
+    --vim.g.notepath = "change here for windows"
 end
-
-vim.g.python3_host_prog = '/home/linuxbrew/.linuxbrew/bin/python3'
 -- Detect filetype and set it to markdown for GoogleKeepNote
 --vim.api.nvim_exec([[
 --  augroup GoogleKeepNoteMarkdown
