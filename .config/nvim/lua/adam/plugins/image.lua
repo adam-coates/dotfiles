c5edd59e332257d5941aea865702f60c45746f6a
adam-coates <adamcoates175@gmail.com>
2024-08-03 22:42:04 +0200

    small update to the image.nvim

--- .config/nvim/lua/adam/plugins/image.lua
+++ .config/nvim/lua/adam/plugins/image.lua
@@ -6,7 +6,7 @@ return {
 			integrations = {
 				markdown = {
 					enabled = true,
-					clear_in_insert_mode = false,
+					clear_in_insert_mode = true,
 					download_remote_images = true,
 					only_render_image_at_cursor = false,
 					filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
