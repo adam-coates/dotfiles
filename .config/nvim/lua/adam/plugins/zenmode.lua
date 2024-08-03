d755309d8771e6b049b38a8b55626ae2652f7785
adam-coates <adamcoates175@gmail.com>
2024-08-03 18:59:02 +0200

    small update

--- .config/nvim/lua/adam/plugins/zenmode.lua
+++ .config/nvim/lua/adam/plugins/zenmode.lua
@@ -1,26 +1,31 @@
 return {
-    "folke/zen-mode.nvim",
-    event = "VeryLazy",
-    dependencies = {
-        "folke/twilight.nvim",
-        "preservim/vim-pencil"
-    },
-    config = function ()
-        require("zen-mode").setup({
-            window = {
-                backdrop = 0.95,
-                width = 80, -- width of the Zen window
-                height = 1, -- height of the Zen window
-                options = {
-                signcolumn = "no", -- disable signcolumn
-                number = false, -- disable number column
-                relativenumber = false, -- disable relative numbers
-                -- cursorline = false, -- disable cursorline
-                -- cursorcolumn = false, -- disable cursor column
-                -- foldcolumn = "0", -- disable fold column
-                -- list = false, -- disable whitespace characters
-                },
-            },
-        })
-    end
+	"folke/zen-mode.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"folke/twilight.nvim",
+		"preservim/vim-pencil",
+	},
+	config = function()
+		require("zen-mode").setup({
+			window = {
+				backdrop = 0.95,
+				width = 80, -- width of the Zen window
+				height = 1, -- height of the Zen window
+				options = {
+					signcolumn = "no", -- disable signcolumn
+					number = false, -- disable number column
+					relativenumber = false, -- disable relative numbers
+					-- cursorline = false, -- disable cursorline
+					-- cursorcolumn = false, -- disable cursor column
+					-- foldcolumn = "0", -- disable fold column
+					-- list = false, -- disable whitespace characters
+				},
+			},
+		})
+		require("twilight").setup({
+			dimming = {
+				alpha = 0.5,
+			},
+		})
+	end,
 }
