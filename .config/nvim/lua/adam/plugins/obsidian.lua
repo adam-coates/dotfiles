c631bd54f09d23733c49f582a311b74f5cc357c9
adam-coates <adamcoates175@gmail.com>
2024-11-09 19:51:54 +0100

    update to obsidian plugin

--- .config/nvim/lua/adam/plugins/obsidian.lua
+++ .config/nvim/lua/adam/plugins/obsidian.lua
@@ -16,7 +16,7 @@ return {
 				path = "~/notes",
 			},
 		},
-		--open_notes_in = "vsplit",
+		open_notes_in = "vsplit",
 		disable_frontmatter = true,
 		ui = { enable = false },
 
@@ -28,5 +28,26 @@ return {
 				end,
 			},
 		},
+        -- Optional, customize how note IDs are generated given an optional title.
+---@param title string|?
+---@return string
+note_id_func = function(title)
+  -- If a title is given, use it as the ID (convert to lowercase, replace spaces with dashes)
+  if title ~= nil then
+    return title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+  else
+    -- If no title is provided, return a default value like 'untitled'
+    return "untitled"
+  end
+end,
+
+-- Optional, customize how note file names are generated given the ID, target directory, and title.
+---@param spec { id: string, dir: obsidian.Path, title: string|? }
+---@return string|obsidian.Path The full path to the new note.
+note_path_func = function(spec)
+  -- Use the `id` (which is just the sanitized title) for the file name.
+  local path = spec.dir / spec.id
+  return path:with_suffix(".md")
+end,
 	},
 }
