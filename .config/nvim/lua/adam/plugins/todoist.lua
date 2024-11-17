604a5dbc8b767e20c722fc29e42d0f72bfd6eb5c
adam-coates <adamcoates175@gmail.com>
2024-11-17 21:42:29 +0100

    offload todoist integration to an actual plugin

--- .config/nvim/lua/adam/plugins/todoist.lua
+++ .config/nvim/lua/adam/plugins/todoist.lua
@@ -1,3 +1,9 @@
 return {
-    "romgrk/todoist.nvim"
+    'adam-coates/todoist.nvim',
+    lazy = false,
+    module = false,  -- Don't try to load as a module
+    rocks = nil,     -- Explicitly disable LuaRocks for this plugin
+    config = function()
+        require('todoist').setup()
+    end
 }
