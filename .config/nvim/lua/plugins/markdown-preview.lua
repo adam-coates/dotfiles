717da7430af97d6855dac4023fafa7e0490eb2ba
Adam Coates <adamcoates175@gmail.com>
2026-02-19 20:14:26 +0100

    small changes to telescope and markdown preview

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -1,22 +1,29 @@
 return {
-	"iamcco/markdown-preview.nvim",
-	version = "0.0.10",
-	cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
-	event = "VeryLazy",
-	ft = { "markdown", "quarto" },
-	config = function()
-		-- Can't get lazy build to work ¯\_(ツ)_/¯
-		-- This first time loading the plugin, there will be a significant delay becuase the function is synchronous, but after that it shouldn't be noticeable
-		-- TODO: Either figure out how to get lazy build to work, or figure out how to make this asynchronous
-		local install_path = vim.fn.stdpath("data") .. "/lazy/markdown-preview.nvim/app"
-		local node_modules = install_path .. "/node_modules"
-		if vim.fn.empty(vim.fn.glob(node_modules)) > 0 then
-			vim.cmd("!cd " .. install_path .. " && npm install && git restore .")
-		end
-
-		-- Options
-		vim.g.mkdp_auto_close = 0
-		vim.g.mkdp_page_title = "${name} - Preview"
-		vim.g.mkdp_echo_preview_url = 1
+	"adam-coates/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	build = "cd app && yarn install",
+	init = function()
+		vim.g.mkdp_filetypes = { "markdown" }
 	end,
+	ft = { "markdown" },
+	-- "iamcco/markdown-preview.nvim",
+	-- version = "0.0.10",
+	-- cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
+	-- event = "VeryLazy",
+	-- ft = { "markdown", "quarto" },
+	-- config = function()
+	-- 	-- Can't get lazy build to work ¯\_(ツ)_/¯
+	-- 	-- This first time loading the plugin, there will be a significant delay becuase the function is synchronous, but after that it shouldn't be noticeable
+	-- 	-- TODO: Either figure out how to get lazy build to work, or figure out how to make this asynchronous
+	-- 	local install_path = vim.fn.stdpath("data") .. "/lazy/markdown-preview.nvim/app"
+	-- 	local node_modules = install_path .. "/node_modules"
+	-- 	if vim.fn.empty(vim.fn.glob(node_modules)) > 0 then
+	-- 		vim.cmd("!cd " .. install_path .. " && npm install && git restore .")
+	-- 	end
+	--
+	-- 	-- Options
+	-- 	vim.g.mkdp_auto_close = 0
+	-- 	vim.g.mkdp_page_title = "${name} - Preview"
+	-- 	vim.g.mkdp_echo_preview_url = 1
+	-- end,
 }
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
 	"nvim-telescope/telescope.nvim",
 	version = "*",
 	dependencies = {
+		{ "nvim-telescope/telescope-ui-select.nvim" },
 		"nvim-lua/plenary.nvim",
 		-- optional but recommended
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
@@ -15,9 +16,22 @@ return {
 			defaults = {
 				path_display = { "truncate " },
 				layout_strategy = "horizontal",
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous,
+						["<C-j>"] = actions.move_selection_next,
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+					},
+				},
+			},
+			extensions = {
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown(),
+				},
 			},
 		})
 		telescope.load_extension("fzf")
+		telescope.load_extension("ui-select")
 		local keymap = vim.keymap
 		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Find string in cwd" })
